801d0ea5cdcc8e8380f485cbc8cdab82f284310f
Rostislav Vatolin <vatolinrp@mail.ru>
2017-02-25 16:50:47 +0300

    serialization added to a model object

--- src/main/java/com/vatolinrp/weather/model/WeatherConditionTO.java
+++ src/main/java/com/vatolinrp/weather/model/WeatherConditionTO.java
@@ -1,9 +1,10 @@
 package com.vatolinrp.weather.model;
 import lombok.Data;
 
+import java.io.Serializable;
 import java.time.ZonedDateTime;
 
-public @Data class WeatherConditionTO {
+public @Data class WeatherConditionTO implements Serializable {
   private String locationKey;
   private Double temperature;
   private ZonedDateTime targetDate;
