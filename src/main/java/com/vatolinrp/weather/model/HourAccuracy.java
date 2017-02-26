af7ff591975c935bf06d37497f85868e48f2cbb6
Rostislav Vatolin <vatolinrp@mail.ru>
2017-02-27 00:09:27 +0300

    hour accuracy class updated

--- src/main/java/com/vatolinrp/weather/model/HourAccuracy.java
+++ src/main/java/com/vatolinrp/weather/model/HourAccuracy.java
@@ -2,10 +2,14 @@ package com.vatolinrp.weather.model;
 
 import lombok.Data;
 
-public @Data class HourAccuracy {
-  private Double percentAccuracy;
-  private Double forecast;
-  private Double current;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+public @Data class HourAccuracy implements Serializable {
   private Integer hour;
-  private String date;
+  private LocalDate date;
+  private String apiType;
+  private String locationKey;
+  private Double expectedTemperature;
+  private Double actualTemperature;
 }
