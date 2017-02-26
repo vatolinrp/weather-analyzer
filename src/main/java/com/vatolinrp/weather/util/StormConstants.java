8ba1bf7ff3a36fe866ed49ffad02bd4440396a1d
Rostislav Vatolin <vatolinrp@mail.ru>
2017-02-27 00:10:41 +0300

    storm constants interface added

---
+++ src/main/java/com/vatolinrp/weather/util/StormConstants.java
@@ -0,0 +1,15 @@
+package com.vatolinrp.weather.util;
+
+public interface StormConstants {
+  String MINSK_CANNONICAL_LOCATION_KEY = "28580";
+  String MAIL_FROM = "vatolinrp@mail.ru";
+  String MAIL_FROM_PASSWORD = System.getenv("MAIL_PASSWORD" );
+  String ACCUWEATHER_API_TYPE = "AW";
+  String DARK_SKY_API_TYPE = "DS";
+  Long SLEEP_TIME = 600000L;
+  String DARK_SKY_API_KEY = "70038fb5200adc067f7bd899890ee0cf";
+  String ACCCUWEATHER_API_KEY = "nlodiXHXlW4DYOOnld3dAGbigT9A6hav";
+  String DARK_SKY_HOST = "api.darksky.net";
+  String ACCUWEATHER_HOST = "dataservice.accuweather.com";
+  Long HOUR = 3600L;
+}
