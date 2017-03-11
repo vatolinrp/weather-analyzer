fdc161a119e4fae7b65d610163ccd94e7e775831
Rostislav Vatolin <vatolinrp@mail.ru>
2017-03-11 20:40:38 +0300

    calculation util class added

---
+++ src/main/java/com/vatolinrp/weather/util/CalculationUtil.java
@@ -0,0 +1,12 @@
+package com.vatolinrp.weather.util;
+
+public class CalculationUtil {
+
+  public static Double getCelsius( Double fahrenheit ) {
+    return (fahrenheit - 32.)/1.8;
+  }
+
+  public static Double round( Double value ) {
+    return (double) Math.round(value * 100) / 100;
+  }
+}
